05981605dc354ccd746aeb1efdd117b585f7d770
Vladimir Kukushkin <vladimir.kukushkin@f-secure.com>
2020-05-06 14:02:49 +0300

    add monorepo workspace and non-shared target

---
+++ someTarget/someTarget/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  someTarget
+//
+//  Created by Kukushkin, Vladimir on 06.05.2020.
+//  Copyright © 2020 kukushechkin. All rights reserved.
+//
+
+import Foundation
+import someComponent
+import anotherComponent
+
+print("someTarget + \(someComponent().text) + \(anotherComponent().text)")
+
